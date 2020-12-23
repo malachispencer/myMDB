387b1d951902520afaf23d9d29f852250b3d0716
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-23 18:55:31 +0000

    export cleanTestDB function to all test files instead of using in jest.config.js

--- test/setup/cleanTestDB.js
+++ test/setup/cleanTestDB.js
@@ -1,11 +1,9 @@
 const pool = require('../../db/dbConnection');
 
 module.exports = async function cleanTestDB() {
-  const sql = `TRUNCATE users, ratings, reviews, watchlist, favourites;`
-
+  const sql = `TRUNCATE ratings, reviews, watchlist, favourites, users`;
+  
   await pool.query(sql, (err, res) => {
     if (err) { console.log(err); }
   });
 }
-
-// cleanTestDB();
