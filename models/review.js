a2f4aafeabc3eb27f91366b8269855e981dedff6
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-24 12:04:59 +0000

    Review.allByUser returns all a user's reviews in descending order

--- models/review.js
+++ models/review.js
@@ -1,9 +1,10 @@
 const pool = require('../db/dbConnection');
 
 class Review {
-  constructor(reviewID, userID, movieID, title, body, time, date) {
+  constructor(reviewID, userID, username, movieID, title, body, time, date) {
     this.reviewID = reviewID;
     this.userID = userID;
+    this.username = username;
     this.movieID = movieID;
     this.title = title;
     this.body = body;
@@ -28,10 +29,13 @@ class Review {
       .query(sql, values)
       .then(res => { return res.rows[0]; })
       .catch(err => console.log(err))
+
+    const reviewerName = await this.#getReviewerName(userID);
       
     return new Review(
       dbResponse.review_id,
       dbResponse.user_id,
+      reviewerName,
       dbResponse.movie_id,
       dbResponse.title,
       dbResponse.body,
@@ -39,6 +43,50 @@ class Review {
       dbResponse.date
     );
   }
+
+  static async allByUser(userID) {
+    const sql = `
+      SELECT review_id, 
+      user_id, movie_id, title, body,
+      to_char(time, 'HH24:MI') as time, 
+      to_char(date, 'DD/MM/YYYY') as date
+      FROM reviews 
+      WHERE user_id = $1 
+      ORDER BY review_id DESC
+    `;
+
+    const values = [userID];
+
+    const userReviews = await pool
+      .query(sql, values)
+      .then(res => { return res.rows; })
+      .catch(err => console.log(err))
+
+    const reviewerName = await this.#getReviewerName(userID);
+
+    return userReviews.map(review => {
+      return new Review(
+        review.review_id,
+        review.user_id,
+        reviewerName,
+        review.movie_id,
+        review.title,
+        review.body,
+        review.time,
+        review.date
+      );
+    });
+  }
+
+  static async #getReviewerName(userID) {
+    const sql = `SELECT username FROM users WHERE user_id = $1`;
+    const values = [userID];
+
+    return await pool
+      .query(sql, values)
+      .then(res => { return res.rows[0].username; })
+      .catch(err => console.log(err))
+  } 
 }
 
 module.exports = Review;
--- test/integration/review.test.js
+++ test/integration/review.test.js
@@ -23,21 +23,21 @@ describe('Review', () => {
   describe('.create', () => {
     test('adds a review to the database', async () => {
       await Review.create(
-        null, 
+        userID, 
         1, 
         'Loved it', 
         'One of the best movies of all time', 
         '10:40'
       );
 
-      const sql = `SELECT * FROM reviews WHERE user_id IS NULL AND movie_id = $1`;
-      const values = [1];
+      const sql = `SELECT * FROM reviews WHERE user_id = $1 AND movie_id = $2`;
+      const values = [userID, 1];
 
       const reviewFromDB = await pool
         .query(sql, values)
-        .then(res => { return res.rows[0] })
+        .then(res => { return res.rows[0]; })
         
-      expect(reviewFromDB.user_id).toBeNull();
+      expect(reviewFromDB.user_id).toBe(userID);
       expect(reviewFromDB.movie_id).toBe('1');
       expect(reviewFromDB.title).toBe('Loved it');
       expect(reviewFromDB.body).toBe('One of the best movies of all time');
@@ -45,7 +45,7 @@ describe('Review', () => {
 
     test('returns a Review instance', async () => {
       const review = await Review.create(
-        null, 
+        userID, 
         1, 
         'Loved it', 
         'One of the best movies of all time', 
@@ -53,12 +53,66 @@ describe('Review', () => {
       );
 
       expect(review.reviewID).toBeDefined();
-      expect(review.userID).toBeNull();
+      expect(review.userID).toBe(userID);
+      expect(review.username).toBe('malachi');
       expect(review.movieID).toBe('1');
       expect(review.title).toBe('Loved it');
       expect(review.body).toBe('One of the best movies of all time');
       expect(review.time).toBe('10:40');
       expect(review.date).toBe('24/12/2020');
+    });
+  });
+
+  describe('.allByUser', () => {
+    test('returns all reviews made by the given user', async () => {
+      await Review.create(
+        userID, 
+        1, 
+        'Loved it', 
+        'One of the best movies of all time', 
+        '11:05'
+      );
+
+      await Review.create(
+        userID, 
+        2, 
+        'Hated it', 
+        'One of the best worst of all time', 
+        '11:06'
+      );
+
+      const userReviews = await Review.allByUser(userID);
+
+      expect(userReviews).toBeInstanceOf(Array);
+      expect(userReviews.length).toBe(2);
+      expect(userReviews[0].username).toBe('malachi');
+      expect(userReviews[1].username).toBe('malachi');
+      expect(userReviews[0].date).toBe('24/12/2020');
+    });
+
+    test('returns the reviews in descending order', async () => {
+      await Review.create(
+        userID, 
+        1, 
+        'Loved it', 
+        'One of the best movies of all time', 
+        '11:05'
+      );
+
+      await Review.create(
+        userID, 
+        2, 
+        'Hated it', 
+        'One of the best worst of all time', 
+        '11:06'
+      );
+
+      const userReviews = await Review.allByUser(userID);
+
+      expect(userReviews[0].movieID).toBe('2');
+      expect(userReviews[1].movieID).toBe('1');
+      expect(userReviews[0].time).toBe('11:06');
+      expect(userReviews[1].time).toBe('11:05');
     });
   });
 });
