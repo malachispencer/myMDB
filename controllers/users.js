159f025959256b2e07e707c8bb0afd50aefb21e7
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-25 16:51:52 +0000

    if user already registered, we send error json back to client

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,15 @@
+const User = require('../models/user');
+
 module.exports = {
   signUp: async (req, res, next) => {
-    console.log('SUCCESS', req.validated.body);
+    const { username, password } = req.validated.body;
+    const email = req.validated.body.email.toLowerCase();
+    const foundUser = await User.findByEmail(email);
+
+    if (foundUser) { 
+      return res.status(403).json({ error: 'Email already registered' });
+    }
+
+    const newUser = await User.create(username, email, password);
   }
 }
