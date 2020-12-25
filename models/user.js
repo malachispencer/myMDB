2b2ac6e8dfc6b6fa487956aec91ff8d7dea98391
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-25 11:21:36 +0000

    User.create tests all passing

--- models/user.js
+++ models/user.js
@@ -1,7 +1,39 @@
 const pool = require('../db/dbConnection');
+const { hashPassword } = require('../utils/password');
 
 class User {
+  constructor(userID, username, email, googleID) {
+    this.userID = userID;
+    this.username = username;
+    this.email = email;
+    this.googleID = googleID;
+  }
 
+  static async create(username, email, password, googleID = null) {
+    const encryptedPassword = await hashPassword(password);
+
+    const sql = `
+      INSERT INTO users 
+      (username, email, password, google_id) 
+      VALUES ($1, $2, $3, $4) 
+      RETURNING user_id, username, 
+      email, google_id;
+      `;
+
+    const values = [username, email, encryptedPassword, googleID];
+
+    const dbResponse = await pool
+      .query(sql, values)
+      .then(res => { return res.rows[0]; })
+      .catch(err => console.log(err))
+
+    return new User(
+      dbResponse.user_id,
+      dbResponse.username,
+      dbResponse.email,
+      dbResponse.google_id
+    );
+  }
 }
 
 module.exports = User;
--- test/integration/user.test.js
+++ test/integration/user.test.js
@@ -4,17 +4,41 @@ const closeTestDB = require('../setup/closeTestDB');
 const User = require('../../models/user');
 
 describe('User', () => {
-  describe('.create', () => {
-    test('it adds a User to the database', async () => {
-      await User.create('malachi', 'm.spencer@makers.com', '2020');
+  let user;
 
-      const userFromDB = pool
+  beforeEach(async () => {
+    await cleanTestDB();
+    user = await User.create('malachi', 'm.spencer@makers.com', '2020');
+  });
+
+  afterAll(async () => {
+    await closeTestDB();
+  });
+
+  describe('.create', () => {
+    test('adds a User to the database', async () => {
+      const userFromDB = await pool
         .query(`SELECT * FROM users WHERE email = $1`, ['m.spencer@makers.com'])
         .then(res => { return res.rows[0]; })
 
       expect(userFromDB.user_id).toBeDefined();
       expect(userFromDB.username).toBe('malachi');
       expect(userFromDB.email).toBe('m.spencer@makers.com');
+    });
+
+    test('hashes password before saving the user', async () => {
+      const passwordFromDB = await pool
+        .query(`SELECT password FROM users WHERE email = $1`, ['m.spencer@makers.com'])
+        .then(res => { return res.rows[0]; })
+
+      expect(passwordFromDB).not.toBe('2020');
+    });
+
+    test('returns a User instance', () => {      
+      expect(user.userID).toBeDefined();
+      expect(user.username).toBe('malachi');
+      expect(user.email).toBe('m.spencer@makers.com');
+      expect(user.googleID).toBeNull();
     });
   });
 });
