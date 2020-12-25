5ea42289b31136c4661ab7b57559f2f8580d274b
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-25 21:01:07 +0000

    Renamed middleware user validation

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require('express-promise-router')();
 const UsersController = require('../controllers/users');
-const { validateUser, userSchema } = require('../middleware/users');
+const { validateUser, userSchema } = require('../middleware/joiUserValidation');
 
 router.route('/')
   .post(validateUser(userSchema), UsersController.signUp);
