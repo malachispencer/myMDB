bab92e4c45a035f9a04eb9a9bfd6a816f8c78a7c
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-27 15:20:48 +0000

    Removed google_id from users table

--- db/migrations/01_create_users_table.sql
+++ db/migrations/01_create_users_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users (user_id SERIAL PRIMARY KEY, username VARCHAR(60) UNIQUE, email VARCHAR(60) UNIQUE, password VARCHAR(255), google_id BIGINT, facebook_id BIGINT);
+CREATE TABLE users (user_id SERIAL PRIMARY KEY, username VARCHAR(60) UNIQUE, email VARCHAR(60) UNIQUE, password VARCHAR(255), facebook_id BIGINT);
