794301489f38aa1c7b31578fc0d7ae8549bcbd40
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-31 10:48:29 +0000

    Rating.update uses ratingID now

--- models/rating.js
+++ models/rating.js
@@ -66,20 +66,20 @@ class Rating {
     });
   }
 
-  static async update(userID, movieID, newScore) {
+  static async update(ratingID, newScore) {
     const sql = `
       UPDATE ratings 
       SET score = $1 
-      WHERE user_id = $2 AND movie_id = $3 
+      WHERE rating_id = $2
       RETURNING rating_id, user_id, movie_id, score
     `;
 
-    const values = [newScore, userID, movieID];
+    const values = [newScore, ratingID];
 
     const newRating = await pool
       .query(sql, values)
       .then(res => { return res.rows[0]; })
-      .catch(err => console.log(err))
+      .catch(err => console.log('RATING.UPDATE ERROR', err))
 
     return new Rating(
       newRating.rating_id,
--- test/models/rating.test.js
+++ test/models/rating.test.js
@@ -79,7 +79,7 @@ describe('Rating', () => {
       const rating = await Rating.create(userID, 1, 7);
       const ratingID = rating.ratingID;
 
-      const updatedRating = await Rating.update(userID, 1, 7);
+      const updatedRating = await Rating.update(ratingID, 7);
 
       expect(rating.ratingID).toEqual(updatedRating.ratingID);
       expect(rating.userID).toEqual(updatedRating.userID);
