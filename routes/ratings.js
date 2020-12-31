b2df0c8a87a559afe27659025c27907f594547ce
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-31 10:48:59 +0000

    Added route and handler to update a rating

--- routes/ratings.js
+++ routes/ratings.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = require('express-promise-router')();
 const passport = require('passport');
 const passportJWT = require('../middleware/passportJWT');
-const { validateBody, ratingSchema } = require('../middleware/joi');
+const { validateBody, ratingSchema, updateRatingSchema } = require('../middleware/joi');
 const RatingsController = require('../controllers/ratings');
 
 router.route('/')
-  .post(passport.authenticate('jwt', { session: false }), validateBody(ratingSchema), RatingsController.create);
+  .post(
+    passport.authenticate('jwt', { session: false }), 
+    validateBody(ratingSchema), 
+    RatingsController.create
+  );
+
+router.route('/')
+  .put(
+    passport.authenticate('jwt', { session: false }),
+    validateBody(updateRatingSchema),
+    RatingsController.update
+  );
+
+router.route('/:ratingID')
+  .delete(
+    passport.authenticate('jwt', { session: false }), 
+    RatingsController.delete
+  );
 
 module.exports = router;
