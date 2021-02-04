9472bcebfabe21658141c633ebbd76a5ab5871ef
Malachi Spencer <m.spencer@live.co.uk>
2021-02-04 08:34:11 +0000

    Created mock api responses

---
+++ __fixtures__/tmdbResponse.js
@@ -0,0 +1,192 @@
+module.exports = {
+  totalPages: {
+    data: {
+      total_pages: 1
+    }
+	},
+	
+  movieIDs: {
+    data: {
+      results: [
+        { id: 1 },
+        { id: 2 }
+      ]
+    }
+	},
+	
+  movieOneDetails: {
+    data: {
+			adult: false,
+			backdrop_path: '/fNG7i7RqMErkcqhohV2a6cV1Ehy.jpg',
+			belongs_to_collection: {
+					id: 2344,
+					name: 'The Matrix Collection',
+					poster_path: '/lh4aGpd3U9rm9B8Oqr6CUgQLtZL.jpg',
+					backdrop_path: '/bRm2DEgUiYciDw3myHuYFInD7la.jpg'
+			},
+			budget: 63000000,
+			genres: [
+					{
+							id: 28,
+							name: 'Action'
+					},
+					{
+							id: 878,
+							name: 'Science Fiction'
+					}
+			],
+			homepage: 'http://www.warnerbros.com/matrix',
+			id: 603,
+			imdb_id: 'tt0133093',
+			original_language: 'en',
+			original_title: 'The Matrix',
+			overview: 'Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.',
+			popularity: 53.252,
+			poster_path: '/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg',
+			production_companies: [
+					{
+							id: 79,
+							logo_path: '/tpFpsqbleCzEE2p5EgvUq6ozfCA.png',
+							name: 'Village Roadshow Pictures',
+							origin_country: 'US'
+					},
+					{
+							id: 372,
+							logo_path: null,
+							name: 'Groucho II Film Partnership',
+							origin_country: ''
+					},
+					{
+							id: 1885,
+							logo_path: '/xlvoOZr4s1PygosrwZyolIFe5xs.png',
+							name: 'Silver Pictures',
+							origin_country: 'US'
+					},
+					{
+							id: 174,
+							logo_path: '/ky0xOc5OrhzkZ1N6KyUxacfQsCk.png',
+							name: 'Warner Bros. Pictures',
+							origin_country: 'US'
+					}
+			],
+			production_countries: [
+					{
+							iso_3166_1: 'AU',
+							name: 'Australia'
+					},
+					{
+							iso_3166_1: 'US',
+							name: 'United States of America'
+					}
+			],
+			release_date: '1999-03-30',
+			revenue: 463517383,
+			runtime: 136,
+			spoken_languages: [
+					{
+							english_name: 'English',
+							iso_639_1: 'en',
+							name: 'English'
+					}
+			],
+			status: 'Released',
+			tagline: 'Welcome to the Real World.',
+			title: 'The Matrix',
+			video: false,
+			vote_average: 8.1,
+			vote_count: 18598
+    }
+	},
+	
+  movieTwoDetails: {
+		data: {
+			adult: false,
+			backdrop_path: '/sDxCd4nt3eR4qOCW1GoD0RabQtq.jpg',
+			belongs_to_collection: {
+					id: 2344,
+					name: 'The Matrix Collection',
+					poster_path: '/lh4aGpd3U9rm9B8Oqr6CUgQLtZL.jpg',
+					backdrop_path: '/bRm2DEgUiYciDw3myHuYFInD7la.jpg'
+			},
+			budget: 150000000,
+			genres: [
+					{
+							id: 12,
+							name: 'Adventure'
+					},
+					{
+							id: 28,
+							name: 'Action'
+					},
+					{
+							id: 53,
+							name: 'Thriller'
+					},
+					{
+							id: 878,
+							name: 'Science Fiction'
+					}
+			],
+			homepage: '',
+			id: 604,
+			imdb_id: 'tt0234215',
+			original_language: 'en',
+			original_title: 'The Matrix Reloaded',
+			overview: `Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, as more and more humans are being freed from the matrix and brought to Zion, the one and only stronghold of the Resistance.  Neo himself has discovered his superpowers including super speed, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition. But a nasty piece of news hits the human resistance: 250,000 machine sentinels are digging to Zion and would reach them in 72 hours. As Zion prepares for the ultimate war, Neo, Morpheus and Trinity are advised by the Oracle to find the Keymaker who would help them reach the Source.  Meanwhile Neo's recurrent dreams depicting Trinity's death have got him worried and as if it was not enough, Agent Smith has somehow escaped deletion, has become more powerful than before and has fixed Neo as his next target.`,
+			popularity: 36.102,
+			poster_path: '/aA5qHS0FbSXO8PxcxUIHbDrJyuh.jpg',
+			production_companies: [
+					{
+							id: 79,
+							logo_path: '/tpFpsqbleCzEE2p5EgvUq6ozfCA.png',
+							name: 'Village Roadshow Pictures',
+							origin_country: 'US'
+					},
+					{
+							id: 1885,
+							logo_path: '/xlvoOZr4s1PygosrwZyolIFe5xs.png',
+							name: 'Silver Pictures',
+							origin_country: 'US'
+					},
+					{
+							id: 172,
+							logo_path: null,
+							name: 'NPV Entertainment',
+							origin_country: 'US'
+					},
+					{
+							id: 174,
+							logo_path: '/ky0xOc5OrhzkZ1N6KyUxacfQsCk.png',
+							name: 'Warner Bros. Pictures',
+							origin_country: 'US'
+					}
+			],
+			production_countries: [
+					{
+							iso_3166_1: 'AU',
+							name: 'Australia'
+					},
+					{
+							iso_3166_1: 'US',
+							name: 'United States of America'
+					}
+			],
+			release_date: '2003-05-15',
+			revenue: 738599701,
+			runtime: 138,
+			spoken_languages: [
+					{
+							english_name: 'English',
+							iso_639_1: 'en',
+							name: 'English'
+					}
+			],
+			status: 'Released',
+			tagline: 'Free your mind.',
+			title: 'The Matrix Reloaded',
+			video: false,
+			vote_average: 7,
+			vote_count: 7536
+	  }
+  }
+}
