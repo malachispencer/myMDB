1562690b0fc4b0a71750e6e0e3b9e2a93db934e6
Malachi Spencer <m.spencer@live.co.uk>
2021-02-04 08:35:08 +0000

    Successfully mocked api responses for search function

--- test/models/movie.test.js
+++ test/models/movie.test.js
@@ -1,11 +1,36 @@
+const testDB = require('../testDB');
 const Movie = require('../../models/movie');
+const tmdbResponse = require('../../__fixtures__/tmdbResponse');
+const mockAxios = require('axios');
+
+jest.mock('axios');
+
+beforeEach(async () => {
+  await testDB.clean();
+});
+
+afterAll(async () => {
+  await testDB.close();
+});
 
 describe('Movie', () => {
   describe('search', () => {
     test('returns results of the search term as array of movies', async () => {
+      mockAxios.get.mockResolvedValueOnce(tmdbResponse.totalPages);
+      mockAxios.get.mockResolvedValueOnce(tmdbResponse.movieIDs);
+      mockAxios.get.mockResolvedValueOnce(tmdbResponse.movieOneDetails);
+      mockAxios.get.mockResolvedValueOnce(tmdbResponse.movieTwoDetails);
+
       const results = await Movie.search('matrix');
-      console.log(results);
-      expect(results).toBeNull();
+      const randIndex = Math.floor(Math.random() * results.length);
+
+      expect(results.length).toBe(2);
+      expect(results).toBeInstanceOf(Array);
+      expect(results[randIndex]).toBeInstanceOf(Movie);
+      expect(results[0].id).toBe(tmdbResponse.movieOneDetails.data.id);
+      expect(results[0].title).toBe(tmdbResponse.movieOneDetails.data.title);
+      expect(results[1].plot).toBe(tmdbResponse.movieTwoDetails.data.overview);
+      expect(results[1].genres).toEqual(tmdbResponse.movieTwoDetails.data.genres);
     });
   });
 });
